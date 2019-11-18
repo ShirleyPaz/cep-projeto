52b4d49062863123dafb2b2cdac9629a872fbb3f
shirleypaz <shirley.paz@gmail.com>
2019-11-18 04:36:57 -0300

    alertas implementados e box de resultado da busca

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,10 +7,43 @@ const App = () => {
   const [address, setAddress] = useState();
   const [query, setQuery] = useState("");
 
+  const getAddress = useCallback(async () => {
+    if (query) {
+      const response = await fetch(`http://localhost:4000/consulta/${query}`)
+        .then(res => {
+          if (res.status > 200) {
+            alert("CEP inválido ou não encontrado");
+          }
+          return res.clone().json();
+        })
+        .catch(err => alert("Erro na chamada, por favor tente mais tarde."));
+
+      if (response.cep) setAddress(response);
+    }
+  }, [query]);
+
+  useEffect(() => {
+    getAddress();
+  }, [query, getAddress]);
+
+  const updateCep = event => {
+    setCep(event.target.value);
+  };
+
+  const formSubmit = event => {
+    event.preventDefault();
+    if (cep.length < 8) {
+      alert("Digite um CEP válido!");
+    } else {
+      setQuery(cep);
+      setCep("");
+    }
+  };
+
   return (
     <main>
       <div className="search-box">
-        <form data-testid="cep-form" onSubmit="">
+        <form data-testid="cep-form" onSubmit={formSubmit}>
           <MaskedInput
             data-testid="cep-input"
             guide={false}
@@ -18,13 +51,33 @@ const App = () => {
             type="text"
             value={cep}
             placeholder="00000-000"
-            onChange=""
+            onChange={updateCep}
           />
           <button className="cep-form__button" type="submit">
             Buscar CEP
           </button>
         </form>
       </div>
+
+      {address && (
+        <div data-testid="address-result" className="address-result">
+          <ul>
+            {Object.keys(address).map(field => (
+              <li key={field} className={`address-result__item`}>
+                <div className="address-result__item--field">
+                  {field.toUpperCase()}:
+                </div>
+                <div
+                  data-testid={`${field}-value`}
+                  className="address-result__item--value"
+                >
+                  {address[field]}
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </main>
   );
 };

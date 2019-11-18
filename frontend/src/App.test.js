8b9adfa447345d3126baed791f4f7e4356c24c02
shirleypaz <shirley.paz@gmail.com>
2019-11-18 04:38:24 -0300

    Teste e2d utlizando testing-library/react

---
+++ frontend/src/App.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { render, fireEvent, waitForElement } from "@testing-library/react";
+
+import App from "./App";
+
+const validCep = "04578910";
+const validCepRes = "04578-910";
+
+const invalidCep = "XXXX";
+const invalidCepRes = "Cep inválido";
+
+const notFoundCep = "00000000";
+const notFoundCepRes = "Cep não foi encontrado na base de dados";
+
+test("return a valid address", async () => {
+  const { container, queryByTestId } = render(<App />);
+
+  const input = queryByTestId("cep-input");
+  const form = queryByTestId("cep-form");
+
+  fireEvent.change(input, { target: { value: "04578910" } });
+  fireEvent.submit(form);
+
+  await waitForElement(() => queryByTestId("cep-value"));
+  expect(queryByTestId("cep-value").childNodes[0]).toMatch(validCepRes);
+});

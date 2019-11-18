d2c58007bde882b098f2db4862c6aaa70996377d
shirleypaz <shirley.paz@gmail.com>
2019-11-18 04:09:01 -0300

    serviço implementado

---
+++ backend/src/services/cepService.js
@@ -0,0 +1,39 @@
+const request = require("request");
+
+exports.getCepService = cep => {
+  const url = `https://viacep.com.br/ws/${cep}/json/`;
+
+  return new Promise(function(resolve, reject) {
+    request(url, (err, res, body) => {
+      // erro ao realizar a chamada
+      if (err) {
+        return reject({ status: res.statusCode, message: err });
+      }
+
+      if (res.statusCode == 400) {
+        // cep inválido
+        return resolve({
+          status: res.statusCode,
+          message: "Cep inválido"
+        });
+      }
+
+      const data = JSON.parse(body);
+      // cep não encontrado
+      if (data.erro) {
+        return resolve({
+          status: 406,
+          message: "Cep não foi encontrado na base de dados"
+        });
+      }
+
+      // cep retornado
+      const { cep, logradouro, localidade, uf } = data;
+      return resolve({
+        status: res.statusCode,
+        message: { cep, logradouro, localidade, uf }
+      });
+
+    });
+  });
+};
